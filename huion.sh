e67e6a05ede37c9e80182bd478a379f43818f2e6
elch01 <53192279+elch01@users.noreply.github.com>
2021-11-15 12:26:21 +0000

    Update huion.sh
    was i drunk?

--- huion.sh
+++ huion.sh
@@ -2,8 +2,10 @@
 
 # this is my personal script, modify it to suit your system/needs!
 
+INITIAL=$(xinput | grep 'Tablet Monitor Pen stylus*')
 OUTPUT=$(echo "$INITIAL" | grep -Eo "id=.." | tr -d 'id=')
 
+INITIAL2=$(xinput | grep 'Tablet Monitor Pad*')
 OUTPUT2=$(echo "$INITIAL2" | grep -Eo "id=.." | tr -d 'id=')
 
 xrandr
